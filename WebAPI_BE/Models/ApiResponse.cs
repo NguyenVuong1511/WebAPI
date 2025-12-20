35457302741a03c5180ca04454a08a5c232d42be
DaoThiThanh <thanhthanhdao178@gmail.com>
2025-12-20 21:17:40 +0700

    Sửa lại code WebAPI

---
+++ WebAPI_BE/Models/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
